3fadfd20e5391a4f8a5e579ed595f64b178f70c9
Niraj Sharma <sharma12345niraj@gmail.com>
2024-08-13 12:43:05 +0545

    userPermission added and normal user are give to comment and like

--- controllers/user/register.controller.js
+++ controllers/user/register.controller.js
@@ -1,10 +1,12 @@
+import { Permission } from "../../model/permission.model.js";
 import { User } from "../../model/user.model.js";
 import bcrypt from "bcrypt";
+import { UserPermission } from "../../model/userPermission.model.js";
 
 const registerUser = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
-    
+
     const userExist = await User.findOne({
       email,
     });
@@ -24,6 +26,29 @@ const registerUser = async (req, res) => {
     });
 
     const userData = await user.save();
+
+    // assign default permissions
+
+    const defaultPermissions = await Permission.find({
+      isDefault: 1,
+    });
+
+    if (defaultPermissions.length > 0) {
+      const permissionArray = [];
+      defaultPermissions.forEach((permission) => {
+        permissionArray.push({
+          permissionName: permission.permissionName,
+          permissionValue: [0, 1, 2, 3],
+        });
+      });
+
+      const userPermission = new UserPermission({
+        userId: userData._id,
+        permissions: permissionArray,
+      });
+
+      await userPermission.save();
+    }
 
     return res.status(200).json({
       success: true,
